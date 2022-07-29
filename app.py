90e77a3964894ea5e3d6a0c48f233c4f9d41cd48
mercaderd <mercaderd@yahoo.es>
2022-07-29 08:07:37 +0200

    last commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import bikesmodel
+import datetime as dt
 
 app = FastAPI()
 
@@ -13,6 +14,23 @@ async def do_train_and_persist():
     return {"success": True}
 
 
-@app.post("/predict")
-async def do_predict():
-    ...
+@app.get("/predict")
+async def do_predict(
+    dteday: dt.date,
+    hr: int,
+    weathersit: str,
+    temp: float,
+    atemp: float,
+    hum: float,
+    windspeed: float,
+):
+    result = bikesmodel.predict(
+        dteday,
+        hr,
+        weathersit,
+        temp,
+        atemp,
+        hum,
+        windspeed,
+    )
+    return {"number_cyclists": result}
--- library/bikesmodel/__init__.py
+++ library/bikesmodel/__init__.py
@@ -1,15 +1,24 @@
 """
-Library code for Washingtong DC bike model
+Library code for Washington DC bike model
 """
 
-__version__="0.1"
+__version__ = "0.1"
+
 
 import os
-import joblib
+
+# import pathlib
 
 import pandas as pd
+
 import numpy as np
-from sklearn.compose import ColumnTransformer, make_column_transformer, make_column_selector
+import joblib
+
+from sklearn.compose import (
+    ColumnTransformer,
+    make_column_transformer,
+    make_column_selector,
+)
 from sklearn.impute import SimpleImputer, KNNImputer
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer
@@ -24,12 +33,7 @@ def ffill_missing(ser):
 
 
 def is_weekend(data):
-    return (
-        data["dteday"]
-        .dt.day_name()
-        .isin(["Saturday", "Sunday"])
-        .to_frame()
-    )
+    return data["dteday"].dt.day_name().isin(["Saturday", "Sunday"]).to_frame()
 
 
 def year(data):
@@ -39,10 +43,13 @@ def year(data):
 
 def train_and_persist() -> None:
     df = pd.read_csv(
-        os.path.join(DATA_DIRECTORY,"hour.csv"), 
-        parse_dates=["dteday"])
+        os.path.join(DATA_DIRECTORY, "hour.csv"),
+        parse_dates=["dteday"],
+    )
+
     X = df.drop(columns=["instant", "cnt", "casual", "registered"])
     y = df["cnt"]
+
     ffiller = FunctionTransformer(ffill_missing)
     weather_enc = make_pipeline(
         ffiller,
@@ -54,46 +61,34 @@ def train_and_persist() -> None:
         (ffiller, make_column_selector(dtype_include=np.number)),
         (weather_enc, ["weathersit"]),
     )
-
-    preprocessing = FeatureUnion([
-        ("is_weekend", FunctionTransformer(is_weekend)),
-        ("year", FunctionTransformer(year)),
-        ("column_transform", ct)
-    ])
-
-    reg = Pipeline([("preprocessing", preprocessing), ("model", RandomForestRegressor())])
+    preprocessing = FeatureUnion(
+        [
+            ("is_weekend", FunctionTransformer(is_weekend)),
+            ("year", FunctionTransformer(year)),
+            ("column_transform", ct),
+        ]
+    )
+    reg = Pipeline(
+        [("preprocessing", preprocessing), ("model", RandomForestRegressor())]
+    )
 
     X_train, y_train = X.loc[X["dteday"] < "2012-10"], y.loc[X["dteday"] < "2012-10"]
-    #X_test, y_test = X.loc["2012-10" <= X["dteday"]], y.loc["2012-10" <= X["dteday"]]
+    # X_test, y_test = X.loc["2012-10" <= X["dteday"]], y.loc["2012-10" <= X["dteday"]]
 
     reg.fit(X_train, y_train)
 
     joblib.dump(reg, "/tmp/model.joblib")
-    
 
-def predict(dteday,
-    hr,
-    weathersit,
-    temp,
-    atemp,
-    hum,
-    windspeed) -> int:
+
+def predict(dteday, hr, weathersit, temp, atemp, hum, windspeed) -> int:
     model = joblib.load("/tmp/model.joblib")
-    X_input = pd.DataFrame([[dteday,
-    hr,
-    weathersit,
-    temp,
-    atemp,
-    hum,
-    windspeed]], columns=[
-    'dteday',
-    'hr',
-    'weathersit',
-    'temp',
-    'atemp',
-    'hum',
-    'windspeed'
-])
+
+    X_input = pd.DataFrame(
+        [[pd.to_datetime(dteday), hr, weathersit, temp, atemp, hum, windspeed]],
+        columns=["dteday", "hr", "weathersit", "temp", "atemp", "hum", "windspeed"],
+    )
+
     result = model.predict(X_input)
+
     return int(result)
 
